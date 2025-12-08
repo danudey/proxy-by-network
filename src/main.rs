e5f4a8c2219707d1de9f12d2edb2dcf43a32c76e
Daniel Fox <danudey@me.com>
2025-12-07 16:11:26 -0800

    More changes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,92 @@
+#[macro_use]
+extern crate log;
+
 mod network_manager;
 mod activeconnections;
 mod dbus;
 
 use zbus::Connection;
+
+use std::io::Read;
+use std::fs::OpenOptions;
+use std::process::exit;
+use std::io::IsTerminal;
+
+use toml::Table;
+
+use log::{error, info, LevelFilter};
+use systemd_journal_logger::JournalLog;
+use env_logger::Builder;
+
 use crate::network_manager::NetworkManagerProxy;
 use crate::activeconnections::ActiveConnectionProxy;
+
+fn get_config(network_id: String) -> Result<(), Box<dyn std::error::Error>> {
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("net.cdslash.proxy-by-net");
+
+    let config_path = xdg_dirs
+        .place_config_file("config.toml")
+        .expect("cannot create configuration directory");
+
+    let mut config_data = String::new();
+
+    let config_file = OpenOptions::new()
+        .read(true)
+        .open(config_path);
+
+    if config_file.is_err() {
+        error!("Unable to load config file: {}", config_file.err().unwrap());
+        return Err("Unable to load config file")?;
+    }
+
+    let _ = config_file.unwrap().read_to_string(&mut config_data);
+
+    let config = config_data.parse::<Table>().unwrap();
+    let net_config = config.get(&network_id);
+
+    if net_config.is_none() {
+        error!("Could not find proxy configuration for network {network_id}");
+        exit(0);
+    } else {
+        let x = net_config.unwrap().get("http").unwrap();
+        let proxy_config = &x.as_str().unwrap().to_owned();
+        info!("Network '{network_id}' has proxy configuration '{proxy_config}'");
+        println!("{proxy_config}");
+        Ok(())
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+    if std::io::stdout().is_terminal() {
+        let mut builder = Builder::new();
+        builder.default_format()
+        .format_timestamp(None)
+        .filter_level(LevelFilter::Debug)
+        .init();
+    } else {
+        JournalLog::new()
+            .unwrap()
+            .with_extra_fields(vec![("VERSION", env!("CARGO_PKG_VERSION"))])
+            .with_syslog_identifier(env!("CARGO_PKG_NAME").to_string())
+            .install().unwrap();
+        log::set_max_level(LevelFilter::Debug);
+    }
+
+
     let connection = Connection::system().await?;
     let network_manager = NetworkManagerProxy::new(&connection).await?;
     let primary_connection = network_manager.primary_connection().await?;
+
     let ac = ActiveConnectionProxy::new(&connection, primary_connection).await?;
     let network_id = ac.id().await?;
 
-    if network_id.starts_with("TeamSweden") {
-        println!("http://10.0.0.2:3142");
-    }
+    info!("Got network ID '{network_id}' from d-bus");
 
     drop(network_manager);
     drop(ac);
 
-    let xdg_dirs = xdg::BaseDirectories::with_prefix("myapp");
+    get_config(network_id)
 
-    Ok(())
 }
