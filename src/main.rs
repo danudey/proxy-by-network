08f623db480508fe6bbb04a1c06988a0eb2c18db
Daniel Fox <danudey@me.com>
2025-12-10 10:42:09 -0800

    Fix up some config handling

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,38 @@ use systemd_journal_logger::JournalLog;
 use crate::network_manager::NetworkManagerProxy;
 use crate::active_connections::ActiveConnectionProxy;
 
+const CONFIG_FILE_GLOBAL: &str ="/etc/proxy-by-network.toml";
+
 fn get_config(network_id: String) -> Result<(), Box<dyn std::error::Error>> {
     let xdg_dirs = xdg::BaseDirectories::with_prefix("net.cdslash.proxy-by-net");
 
-    let config_path = xdg_dirs
-        .place_config_file("config.toml")
-        .expect("cannot create configuration directory");
-
+    let config_path_user = xdg_dirs
+        .place_config_file("config.toml").unwrap_or("".into());
     let mut config_data = String::new();
 
-    let config_file = OpenOptions::new()
-        .read(true)
-        .open(config_path);
+    // let config_file_paths = [config_path, "/etc/proxy-by-network.toml"];
 
-    if config_file.is_err() {
-        error!("Unable to load config file: {}", config_file.err().unwrap());
-        return Err("Unable to load config file")?;
-    }
+    let config_file_user = OpenOptions::new()
+            .read(true)
+            .open(config_path_user.clone());
+    let config_file_global = OpenOptions::new()
+            .read(true)
+            .open(CONFIG_FILE_GLOBAL);
 
-    let _ = config_file.unwrap().read_to_string(&mut config_data);
+    let mut config_file = if config_file_user.is_ok() {
+        info!("Loading config file {}", config_path_user.to_str().unwrap());
+        config_file_user.unwrap()
+    } else {
+        info!("Loading config file {}", CONFIG_FILE_GLOBAL);
+        config_file_global.unwrap()
+    };
+
+    // if config_file.is_err() {
+    //     error!("Unable to load config file: {}", config_file.err().unwrap());
+    //     return Err("Unable to load config file")?;
+    // }
+
+    let _ = config_file.read_to_string(&mut config_data);
 
     let config = config_data.parse::<Table>().unwrap();
     let net_config = config.get(&network_id);
